a708b32ccb95028981424e9ee0a9a99ca1044120
sokolserg <sergsokolkin@gmail.com>
2023-11-22 22:37:45 +0300

    Implement NSTableViewDataSource, NSTableViewDelegate in ViewController; add shouldSelectRow method

--- 06_Project2_BullsAndCows/06_Project2_BullsAndCows/ViewController.swift
+++ 06_Project2_BullsAndCows/06_Project2_BullsAndCows/ViewController.swift
@@ -7,24 +7,52 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
+    
     @IBOutlet var tableView: NSTableView!
     @IBOutlet var guess: NSTextField!
     
+    var answer = ""
+    var guesses = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
-        // Update the view, if already loaded.
+            // Update the view, if already loaded.
         }
     }
     
     @IBAction func submitGuess(_ sender: Any) {
     }
-
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return guesses.count
+    }
+    
+    func result(for guess: String) -> String {
+        return "Result"
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
+        
+        if (tableColumn?.title == "Guess") {
+            // this is the Guess column, show a previous guess
+            vw.textField?.stringValue = guesses[row]
+        } else {
+            // this is the Result colunb, call our new method
+            vw.textField?.stringValue = result(for: guesses[row])
+        }
+        
+        return vw
+    }
+    
+    func tableView(_ tableView: NSTableView, shouldSelectRow row: Int) -> Bool {
+        return true
+    }
+    
 }
